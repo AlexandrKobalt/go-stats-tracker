6d5c83ac666230527e0c53e210a198d4d8c5fe29
Alexandr Kovalchuk <>
2023-07-25 16:27:46 +0300

    Ещё изменения. Один хер перезалью реп потом

--- cmd/main.go
+++ cmd/main.go
@@ -4,20 +4,20 @@ import (
 	"fmt"
 	"net/http"
 
-	"github.com/AlexandrKobalt/go-stats-tracker/pkg"
+	api "github.com/AlexandrKobalt/go-stats-tracker/pkg/api"
 	"github.com/go-chi/chi"
 )
 
 func main() {
 	r := chi.NewRouter()
-	r.Use(pkg.StatsMiddleware)
+	r.Use(api.StatsMiddleware)
 
 	r.Post("/post", func(w http.ResponseWriter, r *http.Request) {
 		fmt.Println("Got POST request!")
 	})
 
 	r.Post("/getStats", func(w http.ResponseWriter, r *http.Request) {
-		stats, err := pkg.GetAllStats()
+		stats, err := api.GetAllStats()
 		if err != nil {
 			panic("it's just impossible, I can't believe it")
 		}
